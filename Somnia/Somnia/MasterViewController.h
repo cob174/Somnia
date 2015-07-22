3d61821e7149ff9fcc7c1157663f3c71f832a50f
cob174 <michaelch174@gmail.com>
2015-07-22 18:25:16 -0400

    Master Project

---
+++ Somnia/Somnia/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Somnia
+//
+//  Created by michael chiong on 7/22/15.
+//  Copyright (c) 2015 psychoSoma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
