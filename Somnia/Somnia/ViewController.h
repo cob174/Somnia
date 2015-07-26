d0f3efe47ee7a5c3ae8f52956f9b19dc107b92e9
cob174 <michaelch174@gmail.com>
2015-07-26 17:39:38 -0400

    Correct Project Type

---
+++ Somnia/Somnia/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Somnia
+//
+//  Created by michael chiong on 7/26/15.
+//  Copyright (c) 2015 psychoSoma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
